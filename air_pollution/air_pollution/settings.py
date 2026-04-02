30ecbe3f552619f1901f8c277eadb16558d5ea00
AnhQuan29112004 <namcaodidong2004@gmail.com>
2026-04-03 01:28:01 +0700

    feat: Add MLflow service to docker-compose and configure tracking URI in settings for air quality project

--- air_pollution/air_pollution/settings.py
+++ air_pollution/air_pollution/settings.py
@@ -263,6 +263,9 @@ KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
 KAFKA_TOPIC = "booking_events"
 KAFKA_GROUP_ID = "booking_service_group"
 
+MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI', 'http://be-air-pollution:5000')
+MLFLOW_EXPERIMENT_NAME = "AirQuality_Hanoi_Realtime"
+
 
 GOOGLE_CLIENT_ID = '601097463713-msqb22gt741bck3n9fr0ci5mo8u4msne.apps.googleusercontent.com'
 
