f6b6e57150f1767bbf76ffe4c70731326a015e2f
AnhQuan29112004 <namcaodidong2004@gmail.com>
2026-04-08 23:46:23 +0700

    fix: update MLFLOW settings to use environment variables and add new model parameters

--- air_pollution/air_pollution/settings.py
+++ air_pollution/air_pollution/settings.py
@@ -263,8 +263,12 @@ KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
 KAFKA_TOPIC = "booking_events"
 KAFKA_GROUP_ID = "booking_service_group"
 
-MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI', 'http://be-air-pollution:5000')
-MLFLOW_EXPERIMENT_NAME = "AirQuality_Hanoi_Realtime"
+MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI', 'http://mlflow:5000')
+MLFLOW_EXPERIMENT_NAME = os.getenv('MLFLOW_EXPERIMENT_NAME', 'AirQuality_Hanoi_Realtime')
+MLFLOW_REGISTERED_MODEL_NAME = os.getenv("MLFLOW_REGISTERED_MODEL_NAME", "pm25_xgboost")
+MODEL_TARGET_COLUMN = os.getenv("MODEL_TARGET_COLUMN", "pm25")
+MODEL_MAE_TARGET = float(os.getenv("MODEL_MAE_TARGET", '8.0'))
+MODEL_IMPROVEMENT_TARGET = float(os.getenv("MODEL_IMPROVEMENT_TARGET", '20.0'))
 
 
 GOOGLE_CLIENT_ID = '601097463713-msqb22gt741bck3n9fr0ci5mo8u4msne.apps.googleusercontent.com'
