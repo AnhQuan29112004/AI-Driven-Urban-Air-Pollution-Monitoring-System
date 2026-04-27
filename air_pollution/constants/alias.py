fcb0358f7c64df024ecf20103fe23bfa108804df
AnhQuan29112004 <namcaodidong2004@gmail.com>
2026-04-28 00:30:31 +0700

    feat: add pollutant columns to Alias class for enhanced data handling

--- air_pollution/constants/alias.py
+++ air_pollution/constants/alias.py
@@ -1,11 +1,15 @@
 class Alias:
     TIME_COLUMN_CANDIDATES = ("timestamp", "datetime", "date", "time", "ds")
+    POLLUTANT_COLUMNS = ("pm25", "pm10", "no2", "co", "o3", "so2", "aqi")
     WEATHER_COLUMNS = ("temperature", "humidity", "wind_speed")
     UCI_COLUMN_ALIASES = {
+    "pm25": ("pm25", "pm2.5", "pm2_5", "pm2_5_aqi"),
+    "pm10": ("pm10", "pm10_aqi"),
     "co": ("co", "co(gt)", "co_gt"),
     "nox": ("nox", "nox(gt)", "nox_gt"),
     "no2": ("no2", "no2(gt)", "no2_gt"),
     "o3": ("o3", "o3(gt)", "o3_gt"),
+    "so2": ("so2", "so2(gt)", "so2_gt"),
     "temperature": ("temperature", "t"),
     "humidity": ("humidity", "rh"),
     "wind_speed": ("wind_speed", "ws"),
